cc888025afcf392f4c7a2b3e81c1e8e273f4470d
Roland Rácz <racz0430@gmail.com>
2022-10-11 01:41:11 +0200

    Implementation of the PedastrianNpc. Less methods and functions, than the CarNpc class.

---
+++ src/AutomatedCar/Models/NpcObjects/PedastrianNpc.cs
@@ -0,0 +1,47 @@
+﻿namespace AutomatedCar.Models.NpcObjects
+{
+    using global::AutomatedCar.Models.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class PedastrianNpc : WorldObject, INpc
+    {
+        private int positionX;
+        private int positionY;
+        private int rotation;
+        private int speed;
+        public PedastrianNpc(int x, int y, string filename, int zindex = 1, bool collideable = false, WorldObjectType worldObjectType = WorldObjectType.Other) : base(x, y, filename, zindex, collideable, worldObjectType)
+        {
+            this.PositionX = x;
+            this.PositionY = y;
+            this.Rotation = rotation;
+            this.speed = speed;
+        }
+
+        public int PositionX { get => positionX; set => this.positionX = value; }
+        public int PositionY { get => positionY; set => this.positionY = value; }
+        public int Rotation { get => rotation; set => this.rotation = value; }
+        public int Speed { get => speed; set => this.speed = value; }
+
+        public void Move()
+        {
+            NpcPedastrianMove();
+        }
+
+        private void NpcPedastrianMove()
+        {
+            // Implements the movement of the pedastrian.
+        }
+
+        public void Rotate()
+        {
+            // Sets the rotation of the pedastrian.
+            // It has to be call in the NpcPedastrianMove method.
+        }
+
+
+    }
+}
